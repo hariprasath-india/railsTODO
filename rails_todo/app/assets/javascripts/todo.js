79e30af1b55b9485f2a03f679b528eccde65a586
Hari Prasath <hariven1998@gmail.com>
2021-04-12 22:53:11 +0530

    rails ajax added

--- rails_todo/app/assets/javascripts/todo.js
+++ rails_todo/app/assets/javascripts/todo.js
@@ -1,2 +1,78 @@
-console.log("Hello from todo js");
 
+function create_todo(){
+    var task_name = document.getElementById('task_name').value;
+    Rails.ajax({
+        url: '/todos',
+        type: 'post',
+        data:
+           'todo[task_name]='+task_name,      
+       success:function(response){ 
+            if(response.message == "success"){
+                var update_parm = `onclick = "update_todo(${response.data.id},'InProgress','Completed')"`;
+                var add = '<div class="task" id = "task_'+response.data.id+'"><span id="taskname">'+response.data.task_name +' </span><button class="inprogress" '+update_parm+'  ><span>In Progress</span></button><button class="delete"  onclick="delete_todo('+ response.data.id+')"><i class="far fa-trash-alt" aria-hidden="true"></i></button></div>'
+                document.getElementById('tasks-Open').innerHTML+=add;   
+                document.getElementById('task_name').value= "";  
+                var x = document.getElementsByTagName("DIV")[2];
+                if (x.id == "error") { 
+                    document.getElementById('error').remove(); 
+                }
+            }else{
+                var add = '<div id="error" class="message-alert">'+response.message+'</div>'
+                document.getElementById('tasks-head').innerHTML+=add;   
+            }
+       } 
+    });
+};
+
+function update_todo(id,stat,next_stat){
+    var task_id = '/todos/'+id;
+    var status = stat;
+    var next_status = next_stat;
+    console.log(`${status} -- ${next_status}`)
+    Rails.ajax({
+        url: task_id,
+        type: 'put',
+        data:
+           'todo[status]='+status,         
+       success:function(response){ 
+            if (response.message == "success") {
+                document.getElementById('task_'+id).remove();
+                if(next_status == "Nil"){
+                    var add = '<div class="task completed" id = "task_'+response.data.id+'"><span id="taskname">'+response.data.task_name +' </span><button class="delete" onclick="delete('+response.data.id+')"><i class="far fa-trash-alt" aria-hidden="true"></i></button></div>'
+                    document.getElementById('tasks-'+status ).innerHTML+=add;
+                }else{
+                    var update_parm = `onclick = "update_todo(${response.data.id},'Completed','Nil')"`;
+                    var add = '<div class="task" id = "task_'+response.data.id+'"><span id="taskname">'+response.data.task_name +' </span><button class="inprogress" '+update_parm+' ><span>'+next_status+'</span></button><button class="delete" onclick="delete_todo('+ response.data.id+')"><i class="far fa-trash-alt" aria-hidden="true"></i></button></div>'
+                    document.getElementById('tasks-'+status ).innerHTML+=add;
+                }
+                var x = document.getElementsByTagName("DIV")[2];
+                if (x.id == "error") { 
+                    document.getElementById('error').remove(); 
+                } 
+            }else{
+                var add = '<div id="error" class="message-alert">'+response.message+'</div>'
+                document.getElementById('tasks-head').innerHTML+=add;   
+            }
+       }
+    });
+}
+
+function delete_todo(id){
+    var task_id = '/todos/'+id;
+    Rails.ajax({
+        url: task_id,
+        type: 'delete',
+        success:function(response){ 
+            if (response.message == "success") {
+                document.getElementById('task_'+id).remove();
+                var x = document.getElementsByTagName("DIV")[2];
+                if (x.id == "error") { 
+                    document.getElementById('error').remove(); 
+                }
+            }else{
+                var add = '<div id="error" class="message-alert">'+response.message+'</div>'
+                document.getElementById('tasks-head').innerHTML+=add;   
+            }
+        }
+    });
+}
--- rails_todo/app/controllers/todos_controller.rb
+++ rails_todo/app/controllers/todos_controller.rb
@@ -1,10 +1,10 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!
   before_action :set_todo, only: %i[ show edit update destroy ]
-  before_action :get_all_todo, only: %i[ index ]
 
   # GET /todos or /todos.json
   def index
+    @todos = Todo.all.where(:user_id => current_user.id).sort_by {|sort| sort.updated_at}
   end
 
   # GET /todos/1 or /todos/1.json
@@ -25,37 +25,35 @@ class TodosController < ApplicationController
   def create
     @todo = Todo.new(todo_params)
     @todo[:user_id] = current_user.id
-    @todo[:status] = 'open'
-    respond_to do |format|
+    @todo[:status]= 'Open'
+    if @todo[:task_name] != ''
       if @todo.save
-        format.html { redirect_to todos_url, notice: "Todo was successfully created." }
-        format.json { head :no_content }
+        render :json => {"message" => "success","data" => @todo}
       else
-        format.html { redirect_to todos_url, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
+        render :json => {"data" => @todo.errors}
       end
+    else
+      render :json => {"message" => "Name not to be blank"}
     end
+      
   end
 
   # PATCH/PUT /todos/1 or /todos/1.json
   def update
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to todos_url, notice: "Todo was successfully updated." }
-        format.json { head :no_content }
-      else
-        format.html { redirect_to todos_url, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
+    if @todo.update(todo_params)
+      
+      render :json => {"message" => "success","data" => @todo}
+    else
+      render :json => {"message" => "Problem in Update Status"}
     end
   end
 
   # DELETE /todos/1 or /todos/1.json
   def destroy
-    @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: "Todo was successfully destroyed." }
-      format.json { head :no_content }
+    if @todo.destroy
+      render :json => {"message" => "success"}  
+    else
+      render :json => {"message" => "Problem in Delete"}
     end
   end
 
@@ -63,10 +61,6 @@ class TodosController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_todo
       @todo = Todo.find(params[:id])
-    end
-
-    def get_all_todo
-      @todos = Todo.all.where(:user_id => current_user.id).sort_by {|sort| sort.updated_at}
     end
 
     # Only allow a list of trusted parameters through.
